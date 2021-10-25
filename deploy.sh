c46bcbc8b6415f778e013b3d885a50971a6baaff
kevinand11 <kevinfizu@gmail.com>
2021-10-25 10:36:31 +0100

    deploy setup

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > dist/CNAME
+
+git checkout gh-pages
+git merge main
+
+# build
+yarn run build
+
+git push
+
+git checkout main
