da94a6cdc8c08f92cd59c00c3b57ab45c6f12f90
kevinand11 <kevinfizu@gmail.com>
2021-10-25 10:31:30 +0100

    setup fb

---
+++ src/components/Form.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="hello">
+		<h1>Hello</h1>
+		<vue-editor
+			v-model="value"
+			placeholder="placeholder"
+			use-custom-image-handler
+		/>
+	</div>
+</template>
+
+<script>
+import { VueEditor } from 'vue3-editor'
+import { ref } from 'vue'
+
+export default {
+	name: 'Form',
+	components: { VueEditor },
+	setup () {
+		const value = ref('')
+		return { value }
+	}
+}
+</script>
---
+++ src/utils/firebase.ts
@@ -0,0 +1,32 @@
+import { initializeApp } from 'firebase/app'
+import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage'
+import { addDoc, collection, getFirestore } from 'firebase/firestore'
+
+const app = initializeApp({
+	apiKey: 'AIzaSyCcVYQQOOhLrKYYbX5IXcf3ltwwZbHDutA',
+	authDomain: 'stranerddevelopment.firebaseapp.com',
+	projectId: 'stranerddevelopment',
+	storageBucket: 'stranerddevelopment.appspot.com',
+	messagingSenderId: '1060370867985',
+	appId: '1:1060370867985:web:739c941e672d22aec34eb0',
+	measurementId: 'G-M06FNQESJP'
+})
+export const storage = getStorage(app)
+export const firestore = getFirestore(app)
+
+export const uploadFile = async (path: string, file: File) => {
+	try {
+		path = `${path}/${Date.now()}_${file.name}`
+		const myRef = ref(storage, path)
+		await uploadBytes(myRef, file)
+		const link = await getDownloadURL(myRef)
+		return { name: file.name, path, link, type: file.type, size: file.size }
+	} catch {
+		throw new Error(`Error uploading ${file.name}`)
+	}
+}
+
+export const saveQuestionToDatabase = async (data: any, path: string) => {
+	const docRef = await addDoc(collection(firestore, `pastQuestions/${path}`), data)
+	return docRef.id
+}
