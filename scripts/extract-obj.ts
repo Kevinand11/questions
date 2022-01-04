35ec812e29daaa48e1b86a53b60a8c6709b4832e
kevinand11 <kevinfizu@gmail.com>
2022-01-04 03:37:33 +0100

    fix

--- scripts/extract-obj.ts
+++ scripts/extract-obj.ts
@@ -12,20 +12,24 @@ const specialFolder = './questions/obj/special'
 const exists = fs.existsSync(rawFolder)
 if (!exists) throw new Error(`Raw folder does not exist: ${rawFolder}`)
 
-const files = fs.readdirSync(rawFolder).map(file => `${rawFolder}/${file}`)
+const files = fs.readdirSync(rawFolder).map((file: string) => `${rawFolder}/${file}`)
 
-files.forEach((file) => {
+files.forEach((file: string) => {
 	const pathname = path.normalize(file).split('/')
 	const filename = pathname[pathname.length - 1].split('.')[0]
-	const { examType, subject, year } = getName(filename.split('-').map((n) => n.trim()))
+	const { examType, subject, year } = getName(filename)
 
-	textract.fromFileWithPath(file, { preserveLineBreaks: true, includeAltText: true }, function (error, text) {
+	textract.fromFileWithPath(file, { preserveLineBreaks: true, includeAltText: true }, function (error: string, text: string) {
 		if (error) throw new Error(error)
+		
+		try {
+			const { special, nonSpecial } = parseContent(text, file, subject, examType, year)
 
-		const { special, nonSpecial } = parseContent(text, file, subject, examType, year)
-
-		if (nonSpecial.length) fs.writeFileSync(`${saveFolder}/${examType}-${subject}-${year}.json`, JSON.stringify(nonSpecial, null, 4))
-		if (special.length) fs.writeFileSync(`${specialFolder}/${examType}-${subject}-${year}.json`, JSON.stringify(special, null, 4))
+			if (nonSpecial.length) fs.writeFileSync(`${saveFolder}/${examType}-${subject}-${year}.json`, JSON.stringify(nonSpecial, null, 4))
+			if (special.length) fs.writeFileSync(`${specialFolder}/${examType}-${subject}-${year}.txt`, special.map((q: any) => `Question ${q.order}`).join('\n'))
+		} catch (e) {
+			console.log(e.message)
+		}
 	})
 
 })
