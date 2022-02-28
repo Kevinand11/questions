cfe14aa7cdb0b75919b60a30c0b8130e67e43302
kevinand11 <kevinfizu@gmail.com>
2022-03-01 00:47:38 +0100

    gh-pages

--- src/shims-vue.d.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-declare module '*. @vue/composition-api' {
-  import type { DefineComponent } from ' @vue/composition-api'
-  const component: DefineComponent<{}, {}, any>
-	export default component
-}
